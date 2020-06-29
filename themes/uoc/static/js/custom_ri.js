5a6e098d38c604610072f143cf6dbc6442d355e9
support-albira <support@albirasolutions.com>
2020-06-29 11:38:52 +0200

    IECISA update on 2020-06-29 11:38:51

--- themes/uoc/static/js/custom_ri.js
+++ themes/uoc/static/js/custom_ri.js
@@ -1,37 +1,34 @@
 "use strict";
 
 $(document).ready(function () {
-    var url = location.href.replace(/\/$/, "");
-    console.log('url cliqued...',url);
-    console.log('location cliqued...',location);
-    if (location.hash) {
-        var hash = url.split("#");
-        $('#myTab a[href="#' + hash[1] + '"]').tab("show");
-        url = location.href.replace(/\/#/, "");
-        history.replaceState(null, null, url);
+  
+    //Fitxa tablist   
+    var tabs = $(".list-group");
+    if(tabs.length > 0){
+    	$(".list-group>li>a").each(function(i){
+    		$(this).click(function(e){
+    			e.preventDefault();
+    			e.stopPropagation();
+    			if(!$(this).hasClass("is-active")){
+    				deactivateTabs();
+    				$(this).addClass("is-active");
+    				$(this).attr('aria-selected',true);
+    				var contentId = $(this).attr("href");
+    				if(!$(contentId).hasClass("is-active")){
+    					$(contentId).addClass("is-active");
+    					$(contentId).attr("aria-hidden", false);
+    				}
+    			}
+    		});
+    	});
     }
-
-    $('a[data-toggle="tab"]').on("click", function () {
-        var newUrl;
-        var hash = $(this).attr("href");
-
-        if (hash == "#tab-content-10") {
-            newUrl = url.split("#")[0] + "/perfil";
-        } else if (hash == "#tab-content-11") {
-            newUrl = url.split("#")[0] + "/recerca";
-        } else if (hash == "#tab-content-12") {
-            newUrl = url.split("#")[0] + "/publicacions";
-        } else if (hash == "#tab-content-13") {
-            newUrl = url.split("#")[0] + "/docencia";
-        } else if (hash == "#tab-content-14") {
-            newUrl = url.split("#")[0] + "/difusio";
-        } else if (hash == "#tab-content-15") {
-            newUrl = url.split("#")[0] + "/contacte";
-        } else {
-            newUrl = url.split("#")[0] + hash;
-        }
-
-        newUrl += "/";
-        history.replaceState(null, null, newUrl);
-    });
 });
+function deactivateTabs(){
+	$(".list-group>li>a").each(function(i){
+		$(this).removeClass("is-active");
+		var contentId = $(this).attr("href");
+		$(contentId).removeClass("is-active");
+		$(contentId).attr("aria-hidden", true);
+		$(this).attr('aria-selected',false);
+	});
+}
